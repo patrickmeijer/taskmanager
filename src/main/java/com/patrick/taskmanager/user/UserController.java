0faadbe356c3e414f83e9da2b1fa9c2027f9c07b
Patrick <patrickmeijer95@gmail.com>
2026-03-12 04:30:49 +0100

    feat: implement UserRepository and basic UserController
    - Added UserRepository with existence checks for username and email
    - Created UserController with basic GET & POST endpoints for testing

---
+++ src/main/java/com/patrick/taskmanager/user/UserController.java
@@ -0,0 +1,29 @@
+package com.patrick.taskmanager.user;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
