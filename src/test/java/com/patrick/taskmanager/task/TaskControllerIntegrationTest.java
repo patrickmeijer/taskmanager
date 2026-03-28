01ece470ca3f1111d4101b2a5eece2e1e6a580aa
Patrick <patrickmeijer95@gmail.com>
2026-03-28 03:30:07 +0100

    fix: initializing user manually in TaskControllerIntegrationTest

--- src/test/java/com/patrick/taskmanager/task/TaskControllerIntegrationTest.java
+++ src/test/java/com/patrick/taskmanager/task/TaskControllerIntegrationTest.java
@@ -1,5 +1,9 @@
 package com.patrick.taskmanager.task;
 
+import com.patrick.taskmanager.user.User;
+import com.patrick.taskmanager.user.UserRepository;
+import com.patrick.taskmanager.user.UserRole;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +14,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,6 +27,24 @@ public class TaskControllerIntegrationTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    public void setup() {
+        userRepository.deleteAll();
+        taskRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("password");
+        user.setRole(UserRole.ROLE_USER);
+        userRepository.save(user);
+    }
 
     @Test
     @WithMockUser(username = "testuser", roles = {"USER"})
